462ca3c9a0d2f1f6a1ef33c775fc8e73923ff30b
claudia <clauu013@gmail.com>
2024-09-28 23:53:34 -0300

    #9 Add função para verificar vitoria

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,6 +6,8 @@ export default function () {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(''));
 
+    const winner = calculateWinner(squares);
+
     function handleClick(i) {
 
         if (squares[i]) {
@@ -20,6 +22,29 @@ export default function () {
           }
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
+    }
+
+    function calculateWinner(squares) {
+        
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+        }
+
+        return null;
     }
 
     return (
