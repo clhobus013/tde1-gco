a87c21d89dfd382c2a3ce72907b3f2b0d3844cc2
claudia <clauu013@gmail.com>
2024-09-28 23:42:22 -0300

    #7 Adiciona validação para nao sobrepor jogadas

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,6 +7,11 @@ export default function () {
     const [squares, setSquares] = useState(Array(9).fill(''));
 
     function handleClick(i) {
+
+        if (squares[i]) {
+            return;
+        }
+
         const nextSquares = squares.slice();
         if (xIsNext) {
             nextSquares[i] = "X";
